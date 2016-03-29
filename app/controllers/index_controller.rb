09d465d44066ec2e17495397dc98dc0ed75ae6ed
Mudream <mudream4869@gmail.com>
2016-03-29 18:33:13 +0800

    add tag seive

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,12 @@
 class IndexController < ApplicationController
   def index
     @books = Book.order("last_update DESC")       
+    @tags = Book.where("tag != ''").uniq.pluck(:tag)
+  end
+
+  def seive
+    @books = Book.where(:tag => params[:tag]).order("last_update DESC")
+    @tags = Book.where("tag != ''").uniq.pluck(:tag)
+    render "index"
   end
 end
