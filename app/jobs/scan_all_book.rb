671ecda2b0eaf3ba44501f45b4c181e4b60076dc
Mudream <mudream4869@gmail.com>
2016-04-24 10:11:04 +0800

    add sfacg

--- app/jobs/scan_all_book.rb
+++ app/jobs/scan_all_book.rb
@@ -10,14 +10,14 @@ class ScanAllBook < ProgressJob::Base
     puts "Scanning at " + now.to_s
 
     @books.each do |book|
-      if book.last_scan == nil or book.last_scan + 1.hour < now
+      if book.last_scan == nil or book.last_scan + 2.hour < now or book.sitename == "sfacg"
         @tag = nil
         if book.sitename == "zizaidu"
           @tag = scan_url_zizaidu(book.spoturl)
         elsif book.sitename == "hbooker"
           @tag = scan_url_hbooker(book.spoturl)
-        elsif book.sitename == "uu"
-          @tag = scan_url_uu(book.spoturl)
+        elsif book.sitename == "sfacg"
+          @tag = scan_url_sfacg(book.spoturl)
         elsif book.sitename == "qidian"
           @tag = scan_url_qidian(book.spoturl)
         end
@@ -52,19 +52,19 @@ private
     return doc.css('span.time-update').text.to_s
   end
 
-  def scan_url_uu(url)
-    require 'open-uri'
-    require 'nokogiri'
-    doc = Nokogiri::HTML(open(url))
-    puts doc
-    return doc.css('div.shijian').length
-  end
-
   def scan_url_qidian(url)
     require 'open-uri'
     require 'nokogiri'
     doc = Nokogiri::HTML(open(url))
     return doc.css('span[itemprop="dateModified"]').text.to_s
+  end
+
+  def scan_url_sfacg(url)
+    require 'open-uri'
+    require 'nokogiri'
+    doc = Nokogiri::HTML(open(url))
+    ret = doc.css('li').to_s
+    return ret
   end
 
 
